aa1db4e017b088a6c1eca75bbaf344085e57730c
BlakeVidebeck <blakevidebeck@gmail.com>
2021-07-22 12:02:38 +1200

    add restaurants services and context to simulate api request & include loading indicator

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { theme } from './src/infrastructure/theme/index';
 import RestaurantsScreen from './src/features/restaurants/screens/RestaurantsScreen';
 import { SafeArea } from './src/components/utility/SafeAreaComponent';
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurantsContext';
 
 const Tab = createBottomTabNavigator();
 
@@ -63,19 +64,21 @@ export default function App() {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
-				<NavigationContainer>
-					<Tab.Navigator
-						screenOptions={createScreenOptions}
-						tabBarOptions={{
-							activeTintColor: 'tomato',
-							inactiveTintColor: 'gray',
-						}}
-					>
-						<Tab.Screen name='Restaurants' component={RestaurantsScreen} />
-						<Tab.Screen name='Map' component={Map} />
-						<Tab.Screen name='Settings' component={Settings} />
-					</Tab.Navigator>
-				</NavigationContainer>
+				<RestaurantsContextProvider>
+					<NavigationContainer>
+						<Tab.Navigator
+							screenOptions={createScreenOptions}
+							tabBarOptions={{
+								activeTintColor: 'tomato',
+								inactiveTintColor: 'gray',
+							}}
+						>
+							<Tab.Screen name='Restaurants' component={RestaurantsScreen} />
+							<Tab.Screen name='Map' component={Map} />
+							<Tab.Screen name='Settings' component={Settings} />
+						</Tab.Navigator>
+					</NavigationContainer>
+				</RestaurantsContextProvider>
 			</ThemeProvider>
 			<ExpoStatusBar style='auto' />
 		</>
---
+++ src/services/restaurants/restaurantsService.js
@@ -0,0 +1,34 @@
+import camelize from 'camelize';
+import { mocks, mockImages } from './mock/index';
+
+export const restaurantsTransform = ({ results = [] }) => {
+	const mappedResults = results.map((restaurant) => {
+		// fill in the restaurant images with mock images
+		restaurant.photos = restaurant.photos.map((p) => {
+			return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+		});
+
+		return {
+			// spead in the restaurant and add 2 properties based on the data if open or closed
+			...restaurant,
+			isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+			isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
+		};
+	});
+
+	return camelize(mappedResults);
+};
+
+export const restaurantsRequest = async (
+	location = '37.7749295,-122.4194155'
+) => {
+	try {
+		const mockData = await mocks[location];
+
+		if (!mockData) throw new Error('no mock found');
+
+		return mockData;
+	} catch (err) {
+		console.log(err);
+	}
+};
