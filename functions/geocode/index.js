87d2d41f24fb126f5aca44beb8c4a1eaf4e2dc06
BlakeVidebeck <blakevidebeck@gmail.com>
2021-08-11 16:15:56 +1200

    setup geocoding to return location data

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,12 +1,31 @@
 const Url = require('url-parse');
+const functions = require('firebase-functions');
 
 const { locations: locationsMock } = require('./geocodeMock');
 
-module.exports.geocodeRequest = (req, res) => {
+module.exports.geocodeRequest = (req, res, client) => {
 	// get the city from the search params (toronto)
-	const { city } = new Url(req.url, true).query;
-	// find the city in the locationsMock and return the json of geometry
-	const locationMock = locationsMock[city.toLowerCase()];
+	const { city, mock } = new Url(req.url, true).query;
+	// if second param is &mock=true it will come through as 'true'
+	if (mock === 'true') {
+		// find the city in the locationsMock and return the json of geometry
+		const locationMock = locationsMock[city.toLowerCase()];
+		res.json(locationMock);
+	}
 
-	res.json(locationMock);
+	client
+		.geocode({
+			params: {
+				address: city,
+				key: functions.config().google.key,
+			},
+			timeout: 1000,
+		})
+		.then((r) => {
+			return res.json(r.data);
+		})
+		.catch((e) => {
+			res.status(400);
+			return res.send(e.response.data.error_message);
+		});
 };
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,15 @@
 const functions = require('firebase-functions');
+const { Client } = require('@googlemaps/google-maps-services-js');
+
 const { geocodeRequest } = require('./geocode');
 const { placesRequest } = require('./places');
 
+const client = new Client({});
+
 exports.geocode = functions.https.onRequest((req, res) => {
-	geocodeRequest(req, res);
+	geocodeRequest(req, res, client);
 });
 
 exports.placesNearby = functions.https.onRequest((req, res) => {
-	placesRequest(req, res);
+	placesRequest(req, res, client);
 });
