09a5f600cdf4fcbdef08d7d99b519a48e366b773
BlakeVidebeck <blakevidebeck@gmail.com>
2021-08-11 16:32:40 +1200

    setup places to connect with google places api

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,11 +1,32 @@
 const Url = require('url-parse');
+const functions = require('firebase-functions');
 
 const { mocks, addMockImage } = require('./mockPlaces');
 
-module.exports.placesRequest = (req, res) => {
-	const { location } = new Url(req.url, true).query;
-	const data = mocks[location];
-	if (data) data.results = data.results.map(addMockImage);
+module.exports.placesRequest = (req, res, client) => {
+	const { location, mock } = new Url(req.url, true).query;
+	if (mock === 'true') {
+		const data = mocks[location];
+		if (data) data.results = data.results.map(addMockImage);
+		res.json(data);
+	}
 
-	res.json(data);
+	client
+		.placesNearby({
+			params: {
+				location,
+				radius: 1500,
+				type: 'restaurant',
+				key: functions.config().google.key,
+			},
+			timeout: 1000,
+		})
+		.then((r) => {
+			r.data.results = r.data.results.map(addMockImage);
+			return res.json(r.data);
+		})
+		.catch((e) => {
+			res.status(400);
+			return res.send(e.response.data.error_message);
+		});
 };
